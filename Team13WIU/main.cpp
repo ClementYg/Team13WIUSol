3846bf420fe28409eb6638cd51c9782e6b8149ec
ClementYg <clementyong20@gmail.com>
2025-08-18 22:47:52 +0800

    adding Framing

---
+++ Team13WIU/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+	for (int i = 0; i < 120; i++) {
+		std::cout << '-';
+	}
+	for (int i = 0; i < 24; i++) {
+		std::cout << std::endl;
+	}
+	for (int i = 0; i < 120; i++) {
+		std::cout << '-';
+	}
+}
