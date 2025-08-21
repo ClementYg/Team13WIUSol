008194ec7990655fc0ded2bc3c08d7b0a25c3323
PohYuJie <252711M@mymail.nyp.edu.sg>
2025-08-21 10:35:40 +0800

    Position update

--- Team13WIU/Position.h
+++ Team13WIU/Position.h
@@ -1,15 +1,9 @@
 #pragma once
 class Position
 {
-private:
-	int row;
-	int col;
-
 public:
-	Position(int row = 0, int col = 0);
-	int getRow() const;
-	int getCol() const;
-	void setPosition(int r, int c);
-
-	bool equals(const Position& other) const;
+	int x;
+	int y;
+	Position();
+	~Position();
 };
