6e6255077abed5079aea89ccaaacb307f50769a5
ClementYg <clementyong20@gmail.com>
2025-08-21 10:50:23 +0800

    Revert "Update main.cpp"
    This reverts commit cca472e168cc523b1ea7a610c7a580543626ca37.

--- Team13WIU/main.cpp
+++ Team13WIU/main.cpp
@@ -2,6 +2,47 @@
 #include <iostream>
 
 int main() {
+	// make sure define every NPC's speech before hand
+	std::vector<std::string> OldVillagerlines =
+	{
+		"Hey hey, small boy!",
+		"I am the Old Villager, before you go I have a quest for you", 
+		"Be gay and come back to me for reward!"
+	};
 
-	return 0;
+	std::vector<std::string> Merchantlines =
+	{
+		"Hey I am a Merchant.",
+		"Do you want to buy anything or sell anything?",
+		"I have all the things in this world!"
+	};
+
+	std::vector<std::string> Masterlines =
+	{
+		"I am your master.",
+		"I am going to teach you basic fighting skills",
+		"Hope you won't get hurt or killed"
+	};
+
+	std::vector<std::string> BMOlines =
+	{
+		"What a strong guy!",
+		"Your arm muscles are perfect!",
+		"Join this competition, if you win I will reward you something crazy"
+	};
+
+	// create NPC type and the informations(name, x, y, speech)
+	NPC villager("Old villager", 5, 1, OldVillagerlines);
+	Merchant clement("Clement", 2, 4, Merchantlines);
+	Master Spencer("Spencer", 3, 4, Masterlines);
+	BMOrganiser KaiZher("KaiZher", 4, 4, BMOlines);
+
+	// make them talk or do something like trade()
+	villager.NPCtalk();
+	clement.NPCtalk();
+	clement.trade();
+	Spencer.NPCtalk();
+	KaiZher.NPCtalk();
+	KaiZher.setX(5);
+
 }
