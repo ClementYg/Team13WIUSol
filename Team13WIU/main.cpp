dac3c518ff5328ec64553e396ae2e5ed2ff39a72
ClementYg <clementyong20@gmail.com>
2025-08-21 10:50:40 +0800

    Reapply "Merge branch 'main' into yujie"
    This reverts commit 4b80f5980eb5cfc810f1c2b26ea4fa0244e069d9.

--- Team13WIU/main.cpp
+++ Team13WIU/main.cpp
@@ -1,5 +1,11 @@
 #include "Game.h"
 #include <iostream>
+#include <string>
+#include "NPC.h"
+#include "BMOrganiser.h"
+#include "Master.h"
+#include "Merchant.h"
+#include "Position.h"
 
 int main() {
 	// make sure define every NPC's speech before hand
