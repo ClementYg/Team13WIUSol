ce08539eb96348c233b0297f0e093cc7ea0a92c6
ClementYg <clementyong20@gmail.com>
2025-08-21 10:50:05 +0800

    Revert "Position update"
    This reverts commit 008194ec7990655fc0ded2bc3c08d7b0a25c3323.

--- Team13WIU/Position.h
+++ Team13WIU/Position.h
@@ -1,9 +1,15 @@
 #pragma once
 class Position
 {
+private:
+	int row;
+	int col;
+
 public:
-	int x;
-	int y;
-	Position();
-	~Position();
+	Position(int row = 0, int col = 0);
+	int getRow() const;
+	int getCol() const;
+	void setPosition(int r, int c);
+
+	bool equals(const Position& other) const;
 };
