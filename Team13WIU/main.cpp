20367648ea13e0da71bc49569c77f1a2100cb501
ClementYg <clementyong20@gmail.com>
2025-08-25 14:32:09 +0800

    Update main.cpp

--- Team13WIU/main.cpp
+++ Team13WIU/main.cpp
@@ -3,6 +3,7 @@
 #include "Item.h"
 #include "Inventory.h"
 #include "Map.h"
+#include "startScreen.h"
 
 #include <string>
 #include "NPC.h"
@@ -11,56 +12,26 @@
 #include "Position.h"
 
 int main() {
-	// make sure define every NPC's speech before hand
-	//std::vector<std::string> OldVillagerlines =
-	//{
-	//	"Hey hey, small boy!",
-	//	"I am the Old Villager, before you go I have a quest for you", 
-	//	"Be gay and come back to me for reward!"
-	//};
+	// STEPS FOR INVENTORY. CREATE(NAME, ITEM, PRICE, QUANTITY)
+	//Item* sword = Item::create("HP_Potion", Item::HP_POT,50, 1); //Declare name, SpecificItem
+	//Item* sword1 = Item::create("Sword", Item::WOOD_SWORD, 50, 1);
 
-	//std::vector<std::string> Merchantlines =
-	//{
-	//	"Hey I am a Merchant.",
-	//	"Do you want to buy anything or sell anything?",
-	//	"I have all the things in this world!"
-	//};
+	//Inventory Bag;
 
-	//std::vector<std::string> Masterlines =
-	//{
-	//	"I am your master.",
-	//	"I am going to teach you basic fighting skills",
-	//	"Hope you won't get hurt or killed"
-	//};
-
-	//std::vector<std::string> BMOlines =
-	//{
-	//	"What a strong guy!",
-	//	"Your arm muscles are perfect!",
-	//	"Join this competition, if you win I will reward you something crazy"
-	//};
-
-	//// create NPC type and the informations(name, x, y, speech)
-	//NPC villager("Old villager", 5, 1, OldVillagerlines);
-	//Merchant clement("Clement", 2, 4, Merchantlines);
-	//Master Spencer("Spencer", 3, 4, Masterlines);
-	//BMOrganiser KaiZher("KaiZher", 4, 4, BMOlines);
-
-	//// make them talk or do something like trade()
-	//villager.NPCtalk();
-	//clement.NPCtalk();
-	//clement.trade();
-	//Spencer.NPCtalk();
-	//KaiZher.NPCtalk();
-	//KaiZher.setX(5);
-
-	// Item* sword = Item::create("sword", Item::WEAPON);
-
-	// Inventory Bag;
-
-	// Bag.addItem(sword); 
+	//Bag.addItem(sword,1); 
+	//Bag.addItem(sword1,1);
+	//sword->useItem();
+	//sword1->useItem();
+	//sword->addDesc("SWORD");
+	//
+	//Bag.requestInventory();
 	
-	// Bag.requestInventory();
+
+	//// STEPS FOR MAP 
+	Map MapObj;
+	MapObj.HarbourMap();
+
+	//startScreen();	
 
 	Game game;
 	game.initGame();
@@ -70,3 +41,42 @@ int main() {
 	}
 
 }
+//                      _
+//                     / \
+//                    /|-|\
+//  ____________     /_|_|_\
+// |            |   /| | | |\
+// |            |  /_|#####|_\               __
+// |       _    | /| |#####| |\   ==         ||
+// |______/ \___|/_|_|_|_|_|_|_\==||=========||=
+//  |_|  /   \  |==============|[___]_[_]_[][_]|
+//    | /     \ |--------------|[__][_][]_[__]_|
+//____|/       \| /-----\|----||***************|___
+//oooo| __      | |     ||    ||^----^^--^--^--|ooo
+//oooo|| o| []  | |     ||    ||               |ooo
+//oooo||  |     | |     ||    ||               |ooo
+//=================================================
+
+
+
+
+
+// kz's design
+//                 ^                 ^
+//         ^      / \     ^^            ^^
+//        / \    /   \   / \          ^/ \^
+//       /   \  /     \ /   \    ^    /   \
+//      /     \/       \     \  / \  /     \
+//     /      /         \     \/   \/       \
+//    /      /___________\     \   /         \
+//   /___________\| /___________\ /___________\
+//        | |     | |    | |/         \| |
+//        | |     | |    | |___________| |
+//        | |     | |    | |    | |    | |
+//        | |     | |    | |    | |    | |
+//        | |     | |    | |    | |    | |
+//=================================================
+//
+//                     P
+//
+//=================================================
