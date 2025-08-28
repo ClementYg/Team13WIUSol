d7f575b8085cfa8786c0bfa144023743d9fdf717
PohYuJie <252711M@mymail.nyp.edu.sg>
2025-08-28 14:10:42 +0800

    .
    .

--- Team13WIU/Game.h
+++ Team13WIU/Game.h
@@ -7,7 +7,7 @@
 class Game
 {
 private:
-	Entity* gameObjects[26];
+	Entity* gameObjects[27];
 	Position playerPos;
 	//Puzzle* RiverPuzzle;
 
@@ -108,6 +108,12 @@ private:
 	bool NarraInsideCave2;
 	bool BKAlive;
 	bool BearAlive;
+	bool HeroAlive;
+
+	bool interactedrock;
+	bool interactedground;
+	bool interactedbird;
+	bool interactedbarrel;
 
 //	Puzzle* RiverPuzzle;
 
